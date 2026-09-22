4ab7efcb9e25804760ae514882124898d879c8f7
moa456811-prog <moa456811@gmail.com>
2026-09-22 14:26:36 +0200

    Route legacy steal-and-hatch-anime-eggs loadstring to protected auto launch

--- hubs/steal-and-hatch-anime-eggs/Hub.lua
+++ hubs/steal-and-hatch-anime-eggs/Hub.lua
@@ -1,1 +1,1 @@
-return loadstring(game:HttpGet("https://rbhasxhkhvldbbsvbmqj.supabase.co/functions/v1/a7dev-loader"))()
+return loadstring(game:HttpGet("https://rbhasxhkhvldbbsvbmqj.supabase.co/functions/v1/a7dev-loader?game=steal-and-hatch-anime-eggs"))()
